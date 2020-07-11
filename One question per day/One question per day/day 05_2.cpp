40f665680d9aae4de99ef10544cc1f2ce4ea2d92
xiaomu00 <xiaomu_Y@163.com>
2020-07-11 08:10:16 +0800

    标题：连续子数组最大和

---
+++ One question per day/One question per day/day 05_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+//////标题：连续子数组最大和
+//////题目描述
+//////一个数组有 N 个元素，求连续子数组的最大和。 例如：[-1, 2, 1]，和最大的连续子数组为[2, 1]，其和为 3
+
+int main()
+{
+	int size;
+	cin >> size;
+	vector<int> nums(size);
+	for (size_t i = 0; i < size; ++i)
+		cin >> nums[i];
+	int result = nums[0];
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		// 计算到num[i]的子数组的最大和
+		sum2 = sum1 >= 0 ? sum1 + nums[i] : nums[i];
+		if (sum2 > result)
+			result = sum2;
+		if (sum2 < 0)
+			sum2 = 0;
+		sum1 = sum2;
+	} 
+	cout << result << endl;
+	return 0;
+}
