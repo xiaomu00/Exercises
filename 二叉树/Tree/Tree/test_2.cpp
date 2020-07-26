fad65ecd59a829fae860eb3cf4ce019bf2dcbb7e
xiaomu00 <xiaomu_Y@163.com>
2020-07-26 22:51:30 +0800

    题目2：二叉树镜像

---
+++ 二叉树/Tree/Tree/test_2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+////ÌâÄ¿£º¶þ²æÊ÷¾µÏñ
+
+struct TreeNode {
+int val;
+struct TreeNode *left;
+struct TreeNode *right;
+TreeNode(int x) :
+val(x), left(NULL), right(NULL) {
+}
+};
+
+
+class Solution {
+public:
+	void Mirror(TreeNode *pRoot) {
+		if (pRoot == NULL)
+			return;
+		TreeNode *temp = pRoot->left;
+		pRoot->left = pRoot->right;
+		pRoot->right = temp;
+		Mirror(pRoot->left);
+		Mirror(pRoot->right);
+	}
+};
+
+
+int main()
+{
+
+
+    return 0;
+}
